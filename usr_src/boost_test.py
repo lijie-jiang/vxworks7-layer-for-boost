1f2dce351dc5488b376cf93a7f07287fbe8b4371
oneuber <owenneuber@outlook.com>
2017-02-24 12:55:56 -0500

    Improved specificity of expect statements

--- usr_src/boost_test.py
+++ usr_src/boost_test.py
@@ -12,6 +12,7 @@ import argparse
 from sys import stdout
 import sys
 import re
+from time import sleep
 
 #from datetime import datetime
 #now = datetime.now() #for a timestamped log file
@@ -41,61 +42,61 @@ try:
     con = tn.expect(["->".encode("UTF-8"), "fail".encode("UTF-8")], 45)
     if con[0] == 0:
         stdout.write("Connected to telnet... \n")
+        stdout.write(con[2].decode("UTF-8"))
     else:
         stdout.write("Failed to connect to telnet \n\n")
 
     tn.write("cmd\r\n".encode("UTF-8"))
-    r = tn.expect(["#".encode("UTF-8")], 45)
-    stdout.write("Executed: " + r[2].decode("UTF-8").replace("\r\n", "\nResult: ", 1).replace("\r\n[vxWorks *]#","[vxWorks *]#") + "\n\n")
+    r = tn.expect(["vxWorks *]#".encode("UTF-8")], 45)
+    stdout.write(r[2].decode("UTF-8"))
     
     tn.write('set env "LD_LIBRARY_PATH={ld_library_path}"\r\n'.format(ld_library_path=ld_library_path).encode("UTF-8"))
-    r = tn.expect(["#".encode("UTF-8")], 45)
-    stdout.write("Executed: " + r[2].decode("UTF-8").replace("\r\n", "\nResult: ", 1).replace("\r\n[vxWorks *]#","\n[vxWorks *]#") + "\n\n")
+    r = tn.expect(["vxWorks *]#".encode("UTF-8")], 45)
+    stdout.write(r[2].decode("UTF-8"))
     
     tn.write("cd {src_dir}/status\r\n".format(src_dir=src_dir).encode("UTF-8"))
-    r = tn.expect(["#".encode("UTF-8")], 45)
-    stdout.write("Executed: " + r[2].decode("UTF-8").replace("\r\n", "\nResult: ", 1).replace("\r\n[vxWorks *]#","\n[vxWorks *]#") + "\n\n")
+    r = tn.expect(["vxWorks *]#".encode("UTF-8")], 45)
+    stdout.write(r[2].decode("UTF-8"))
     to_run = "run -x -t 0x01000000 -u 100000 -- "
     for arg in args.argv:
         to_run += arg.replace("\\","/") + " "
     to_run += "\r\n"
     tn.write(to_run.encode("UTF-8"))
-    #tn.write("\r\n".encode("UTF-8"))
-    test = tn.expect(["due to signal 6.".encode("UTF-8"), "due to signal 11.".encode("UTF-8"), "and has been deleted.".encode("UTF-8"), "#".encode("UTF-8")], 100)
+    #sleep(1)
+    test = tn.expect(["due to signal 6.".encode("UTF-8"), "due to signal 11.".encode("UTF-8"), "and has been deleted.".encode("UTF-8"), "vxWorks *".encode("UTF-8")], 100)
     result = test[0] #will give the index of which ever thing our expeect found (-1 for timeout)
     if result != 3:
-        stdout.write("\n\nAn error occured when trying to execute: " + to_run + "The error was:\n\t\t\t")
+        stdout.write("\n\nAn error occured when trying to execute: " + to_run + "info:\n" + test[2].decode("UTF-8"))
     if result == -1:
         stdout.write("After trying to run, we timed out... Rebooting...\n")
         tn.write("\x18".encode("UTF-8")) #resart the telnet session
-        from time import sleep
         sleep(25)
         exited = True
         raise ValueError("We timed out and rebooted the session")
     elif result == 0:
-        stdout.write("SIGABRT related error \n")
         raise ValueError("SIGABRT")
     elif result == 1:
-        stdout.write("SEGFAULT related error \n")
         raise ValueError("SEGFAULT")
     elif result == 2:
-        stdout.write("Something has been deleted \n")
         raise ValueError("Something was deleted")
     #else '#', which is what we want to see
-    stdout.write("Executed: " + test[2].decode("UTF-8").replace("\r\n", "\nResult: ", 1).replace("\r\n[vxWorks *]#","\n[vxWorks *]#") + "\n\n")
+    stdout.write(test[2].decode("UTF-8"))
     tn.write("echo \$?\r\n".encode("UTF-8"))
-    echo_output = tn.read_until("#".encode("UTF-8"), 60).decode("UTF-8")
-    stdout.write("Executed: " + echo_output.replace("\r\n", "\nResult: ", 1).replace("\r\n[vxWorks *]#","\n[vxWorks *]#") + "\n\n") #output the last bit of output before exiting
+    echo_output = tn.read_until("vxWorks *]#".encode("UTF-8"), 60).decode("UTF-8")
+    stdout.write(echo_output) #output the last bit of output before exiting
     echo_value = int(re.findall(r'\d+', echo_output)[0]) #finds the first integer inside the echo string
                 
 except Exception as error:
     stdout.write("\n\nAn error occured:\n\t\t\t" + str(error) + "\n\n")
+    if not tn: #if we failed before telnet was even defined
+        stdout.write("Telnet connection error. Ensure your BOOST_TELNET_ADDR environment variable is correct.")
+        sys.exit(-1)
     tn.write("exit\r\n".encode("UTF-8"))
     stdout.write("Telnet disconnected...")
     stdout.close()
     sys.exit(-1)
 if not exited: #if we did not timeout. If we did, we do not want to stop things at the base terminal level
     tn.write("exit\r\n".encode("UTF-8"))
-stdout.write("Telnet disconnected...")
+stdout.write("\nTelnet disconnected...")
 stdout.close()
 sys.exit(echo_value)
